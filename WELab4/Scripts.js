813e8fe9f5d3aed44d768a7a3fa9bae1c7ab6a40
-v <20b81a0596@cvr.ac.in>
2023-02-15 11:43:31 +0530

    Some Changes Made

---
+++ WELab4/Scripts.js
@@ -0,0 +1,2 @@
+import { app } from "fbconf.js";
+console.log(app);
---
+++ WELab4/fbconf.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAPhl-4TAiGjQnmoGSyDdTuv9Qm0Dq6hlY",
+    authDomain: "test-3485a.firebaseapp.com",
+    databaseURL: "https://test-3485a-default-rtdb.firebaseio.com",
+    projectId: "test-3485a",
+    storageBucket: "test-3485a.appspot.com",
+    messagingSenderId: "842497848536",
+    appId: "1:842497848536:web:d51f188d9b5397f0ae13ad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
